41f2215122f6c1fc12026010a21407ee6eb771a9
Murray Wynnes <mrwynnes@gmail.com>
2016-01-27 23:59:14 +0000

    day17 part1

--- day17/part1.py
+++ day17/part1.py
@@ -10,3 +10,14 @@ for line in content:
     line = line.replace("\n","")
     numbers.append(int(line))
 
+def findsets(list):
+    sets = []
+    for x in range(len(list)):
+        for i in list:
+            if i not in sets:
+                sets.append(i)
+    return sets
+
+numbers = [1,2,3]
+
+print(findsets(numbers))
