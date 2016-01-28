ec58d787b761b8699d96264aebd7b3ec91d6e282
Murray Wynnes <mrwynnes@gmail.com>
2016-01-28 01:05:21 +0000

    day 17 part 1 python

--- day17/part1.py
+++ day17/part1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+import itertools
+import sys
 
 with open("input.txt") as f:
     content = f.readlines()
@@ -7,17 +9,29 @@ numbers = []
 
 # Read the numbers from the input file into array
 for line in content:
-    line = line.replace("\n","")
+    line = line.replace("\n", "")
     numbers.append(int(line))
 
-def findsets(list):
-    sets = []
-    for x in range(len(list)):
-        for i in list:
-            if i not in sets:
-                sets.append(i)
-    return sets
+print numbers
+stuff = numbers
+i = 0
+value = 150
+totalCount = 0
+for L in range(0, len(stuff) + 1):
+    for subset in itertools.combinations(stuff, L):
+        # print subset
+        t = 0
+        itemIndex = 0
+        stopCounting = False
+        for i in subset:
+            itemIndex += 1
+            if(stopCounting == True):
+                break
+            t += i
+            if t > value:
+                stopCounting = True
+            elif t == value and itemIndex == len(subset):
+                totalCount += 1
+                stopCounting = True
 
-numbers = [1,2,3]
-
-print(findsets(numbers))
+print totalCount
